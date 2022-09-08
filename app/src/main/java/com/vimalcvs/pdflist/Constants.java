b671e7a1d1c4ac9331f6a4c7aa0e489f84aa52ca
vimalcvs <vimalcvs29@gmail.com>
2022-09-08 15:34:33 +0530

    commit📃

---
+++ app/src/main/java/com/vimalcvs/pdflist/Constants.java
@@ -0,0 +1,8 @@
+package com.vimalcvs.pdflist;
+
+public class Constants {
+
+    //pdf url
+    public static final String PDF_URL = "https://raw.githubusercontent.com/vimalcvs/Dark-Light-Dialog/main/vk.json";
+
+}
